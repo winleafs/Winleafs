28284d5d347f4e91d7e4f9920fa873150839a2b2
Bart de Bever <bartdebever112@gmail.com>
2019-01-06 22:19:28 +0100

    Added indentify to layout endpoint, not entire correct but good enough

--- Nanoleaf-Api/Endpoints/Interfaces/ILayoutEndpoint.cs
+++ Nanoleaf-Api/Endpoints/Interfaces/ILayoutEndpoint.cs
@@ -14,5 +14,11 @@ namespace Nanoleaf_Api.Endpoints.Interfaces
         /// </summary>
         /// <returns>The current Nanoleaf layout.</returns>
         Task<Layout> GetLayout();
+
+        /// <summary>
+        /// Causes the panels to flash in unison. This is typically used to help users differentiate between multiple panels.
+        /// </summary>
+        /// <returns>An awaitable task.</returns>
+        Task Identify();
     }
 }
