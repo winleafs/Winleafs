ddb1d07f943435cc053efed35f9b00e059176ded
Bart de Bever <bartdebever112@gmail.com>
2019-01-06 18:16:47 +0100

    Layout API interface and models

---
+++ Nanoleaf-Api/Endpoints/Interfaces/ILayoutEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nanoleaf_Api.Models.Layouts;
+
+namespace Nanoleaf_Api.Endpoints.Interfaces
+{
+    public interface ILayoutEndpoint
+    {
+        /// <summary>
+        /// Gets the current Nanoleaf layout.
+        /// </summary>
+        /// <returns>The current Nanoleaf layout.</returns>
+        Task<Layout> GetLayout();
+    }
+}
---
+++ Nanoleaf-Api/Models/Layouts/PanelPosition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace Nanoleaf_Api.Models.Layouts
+{
+    public class PanelPosition
+    {
+        [JsonProperty("panelId")]
+        public int PanelId { get; set; }
+
+        [JsonProperty("x")]
+        public int X { get; set; }
+
+        [JsonProperty("Y")]
+        public int Y { get; set; }
+
+        [JsonProperty("o")]
+        public int Orientation { get; set; }
+    }
+}
