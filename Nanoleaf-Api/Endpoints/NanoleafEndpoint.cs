73b3e9481c966abed564b743033278693db0b2dd
Bart de Bever <bartdebever112@gmail.com>
2019-01-20 23:54:08 +0100

    Added API and API test project.

---
+++ Nanoleaf-Api/Endpoints/NanoleafEndpoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+using RestSharp;
+
+namespace Nanoleaf_Api.Endpoints
+{
+    public abstract class NanoleafEndpoint
+    {
+        protected NanoleafClient Client { get; set; }
+
+        protected async Task<T> SendRequest<T>(string endpoint, Method method, object body = null)
+        {
+            return (T)await SendRequest(endpoint, method, typeof(T), body);
+        }
+
+        protected async Task<object> SendRequest(string endpoint, Method method, Type returnType = null, object body = null)
+        {
+            var restClient = new RestClient(Client._baseUri);
+            var request = new RestRequest($"api/v1/{Client._token}{endpoint}", method);
+            if (body != null)
+            {
+                request.AddJsonBody(body);
+            }
+
+            var response = await restClient.ExecuteTaskAsync(request);
+
+            return returnType == null ? null : JsonConvert.DeserializeObject(response.Content, returnType);
+        }
+    }
+}
---
+++ Nanoleaf-Api/NanoleafClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nanoleaf_Api.Endpoints;
+using Nanoleaf_Api.Endpoints.Interfaces;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using RestSharp;
+
+namespace Nanoleaf_Api
+{
+    public interface INanoleafClient
+    {
+        /// <summary>
+        /// Authorizes with the Nanoleaf device.
+        /// </summary>
+        Task AuthorizeAsync(string ip, int port);
+
+        IEffectsEndpoint EffectsEndpoint { get; }
+    }
+
+    public class NanoleafClient : INanoleafClient
+    {
+        internal Uri _baseUri;
+
+        internal string _token = "";
+
+        public async Task AuthorizeAsync(string ip, int port)
+        {
+            // Rework to own endpoint.
+            _baseUri = new Uri($"http://{ip}:{port}");
+            var client = new RestClient(_baseUri);
+            var request = new RestRequest("api/v1/new", Method.POST);
+            var response = await client.ExecuteTaskAsync(request);
+            var jObject = JObject.Parse(response.Content);
+            _token = jObject["auth_token"].ToString();
+        }
+
+        private IEffectsEndpoint _effectsEndpoint;
+
+        // Don't like this style and want to rework it.
+        public IEffectsEndpoint EffectsEndpoint
+        {
+            get
+            {
+                if (_effectsEndpoint == null)
+                {
+                    _effectsEndpoint = new EffectsEndpoint(this);
+                }
+
+                return _effectsEndpoint;
+            }
+        }
+    }
+}
