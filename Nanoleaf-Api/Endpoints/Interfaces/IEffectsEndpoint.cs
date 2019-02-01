5c348a1e333063519505c6b4527a47ad482fe057
Bart de Bever <Bart.dbever@hetnic.nl>
2019-02-01 15:34:46 +0100

    Effects sync methods

--- Nanoleaf-Api/Endpoints/Interfaces/IEffectsEndpoint.cs
+++ Nanoleaf-Api/Endpoints/Interfaces/IEffectsEndpoint.cs
@@ -13,11 +13,23 @@ namespace Winleafs.Api.Endpoints.Interfaces
         /// <returns>A list of values that represent available effects.</returns>
         Task<IEnumerable<string>> GetEffectsListAsync();
 
+		/// <summary>
+		/// Gets a list of all available effects.
+		/// </summary>
+		/// <returns>A list of values that represent available effects.</returns>
+		IEnumerable<string> GetEffectsList();
+
         /// <summary>
         /// Gets the currently selected effect.
         /// </summary>
         /// <returns>A value representing the currently selected effect.</returns>
         Task<string> GetSelectedEffectAsync();
+
+		/// <summary>
+		/// Gets the currently selected effect.
+		/// </summary>
+		/// <returns>A value representing the currently selected effect.</returns>
+		string GetSelectedEffect();
 
         /// <summary>
         /// Set the current effect to the Nanoleaf.
@@ -26,11 +38,24 @@ namespace Winleafs.Api.Endpoints.Interfaces
         /// <returns>An awaitable task.</returns>
         Task SetSelectedEffectAsync(string effectName);
 
+		/// <summary>
+		/// Set the current effect to the Nanoleaf.
+		/// </summary>
+		/// <param name="effectName">The name of the effect wanting to be set.</param>
+		void SetSelectedEffect(string effectName);
+
         /// <summary>
         /// Gets the details of an effect based on the name.
         /// </summary>
         /// <param name="effectName">The name of the effect wanting to be gotten.</param>
         /// <returns>The details about the effect.</returns>
         Task<Effect> GetEffectDetailsAsync(string effectName);
+
+		/// <summary>
+		/// Gets the details of an effect based on the name.
+		/// </summary>
+		/// <param name="effectName">The name of the effect wanting to be gotten.</param>
+		/// <returns>The details about the effect.</returns>
+		Effect GetEffectDetails(string effectName);
     }
 }
