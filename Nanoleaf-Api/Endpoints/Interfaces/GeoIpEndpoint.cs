0901aa45bab87f27e26eb028c447e7d9cbb5dbb3
Bart de Bever <bartdebever112@gmail.com>
2019-02-06 09:51:40 +0100

    Added implementation

---
+++ Nanoleaf-Api/Endpoints/Interfaces/GeoIpEndpoint.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+using Winleafs.Models.Models.GeoIp;
+
+namespace Winleafs.Api.Endpoints.Interfaces
+{
+    public class GeoIpEndpoint : IGeoIpEndpoint
+    {
+        private const string IPAPI = "http://icanhazip.com";
+        private static readonly Uri GeopIpUri = new Uri("http://ip-api.com/");
+        
+        public GeoIpResult GetGeoIpData()
+        {
+            var ip = GetIp();
+            var client = new RestClient(GeopIpUri);
+            var request = new RestRequest($"json/{ip}");
+            var response = client.Execute(request, Method.GET);
+            if (!response.IsSuccessful || string.IsNullOrWhiteSpace(response.Content))
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<GeoIpResult>(response.Content);
+        }
+
+        public async Task<GeoIpResult> GetGeoIpDataAsync()
+        {
+            var ip = await GetIpASync();
+            var client = new RestClient(GeopIpUri);
+            var request = new RestRequest($"json/{ip}");
+            var response = await client.ExecuteTaskAsync(request);
+            if (!response.IsSuccessful || string.IsNullOrWhiteSpace(response.Content))
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<GeoIpResult>(response.Content);
+        }
+
+        private string GetIp()
+        {
+            return new WebClient().DownloadString(IPAPI);
+        }
+
+        private Task<string> GetIpASync()
+        {
+            return new WebClient().DownloadDataAsync(IPAPI);
+        }
+    }
+}
