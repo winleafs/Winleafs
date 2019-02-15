a051cd714d6531093b796722429a39374f0efb06
Bart de Bever <bartdebever112@gmail.com>
2019-02-15 21:39:18 +0100

    Very basic popup window

---
+++ Nanoleaf-wpf/Views/Popup/Popup.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Winleafs.Wpf.Views.Popup
+{
+    /// <summary>
+    /// Interaction logic for Popup.xaml
+    /// </summary>
+    public partial class Popup : Window
+    {
+        public Popup(string content, string title)
+        {
+            InitializeComponent();
+            this.Title = title;
+            this.PopupContent.Text = content;
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
