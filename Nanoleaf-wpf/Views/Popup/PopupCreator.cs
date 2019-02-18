32435c1f42e62fb16ace3530c02f80f17a449599
Bart de Bever <bartdebever112@gmail.com>
2019-02-18 11:48:02 +0100

    Added resources and class to create popups

---
+++ Nanoleaf-wpf/Views/Popup/PopupCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winleafs.Wpf.Views.Popup
+{
+    public static class PopupCreator
+    {
+        public static void CreatePopup(string title, string body)
+        {
+            new Popup(body, title).Show();
+        }
+
+        public static void CreateErrorPopup(string body)
+        {
+            CreatePopup(PopupResource.Error, body);
+        }
+    }
+}
