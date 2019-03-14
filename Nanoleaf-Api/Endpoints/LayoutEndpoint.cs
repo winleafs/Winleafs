2ab2a187c918ba00efe12b6f3def7dc406baa13e
Bart de Bever <bartdebever112@gmail.com>
2019-03-14 22:52:44 +0100

    Implemented the endpoint

---
+++ Nanoleaf-Api/Endpoints/LayoutEndpoint.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Winleafs.Api.Endpoints.Interfaces;
+using Winleafs.Models.Models.Layouts;
+
+namespace Winleafs.Api.Endpoints
+{
+    public class LayoutEndpoint : NanoleafEndpoint, ILayoutEndpoint
+    {
+        /// <inheritdoc />
+        public Layout GetLayout()
+        {
+            return GetLayoutAsync().GetAwaiter().GetResult();
+        }
+
+        /// <inheritdoc />
+        public Task<Layout> GetLayoutAsync()
+        {
+            return SendRequest<Layout>("panelLayout/layout", Method.GET);
+        }
+
+        /// <inheritdoc />
+        public void Identify()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task IdentifyAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
