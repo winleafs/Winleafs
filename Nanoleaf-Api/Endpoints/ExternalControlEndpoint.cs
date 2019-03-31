7f1e37039c57501451e6fa6b09067d182f9dfc8b
Stijn Oostdam <stijn.oostdam@xs4all.nl>
2019-03-31 16:53:57 +0200

    Fixed comments

--- Nanoleaf-Api/Endpoints/ExternalControlEndpoint.cs
+++ Nanoleaf-Api/Endpoints/ExternalControlEndpoint.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
 
 using RestSharp;
--- Nanoleaf-Api/Endpoints/Interfaces/IExternalControlEndPoint.cs
+++ Nanoleaf-Api/Endpoints/Interfaces/IExternalControlEndPoint.cs
@@ -8,7 +8,7 @@ namespace Winleafs.Api.Endpoints.Interfaces
         /// <summary>
         /// Gets the needed info to start external control
         /// </summary>
-        /// <returns>Awiatable task<<see cref="ExternalControlInfo"/>><returns>
+        /// <returns>Awaitable task of type <<see cref="ExternalControlInfo"/>><returns>
         Task<ExternalControlInfo> GetExternalControlInfoAsync();
 
         /// <summary>
@@ -24,14 +24,14 @@ namespace Winleafs.Api.Endpoints.Interfaces
         Task PrepareForExternalControl();
 
         /// <summary>
-        /// 
+        /// Sets the color of an individual panel via UDP. Only works if external control is activated
         /// </summary>
         /// <param name="panelId">ID of the panel</param>
         /// <param name="red">The red RDB value</param>
         /// <param name="green">The green RDB value</param>
         /// <param name="blue">The blue RDB value</param>
         /// <param name="transitionTime">The time to transition to this frame from the previous frame (must be equal or greater than 1), default 1</param>
-        /// <returns>waitable task</returns>
+        /// <returns>Awaitable task</returns>
         Task SetPanelColorAsync(int panelId, int red, int green, int blue, int transitionTime = 1);
     }
 }
