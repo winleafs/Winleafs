816459ad72a715dcd9ede0e33761e5fd39352c72
Bart de Bever <bartdebever112@gmail.com>
2019-08-06 23:08:04 +0200

    Enum localizer  doc

--- Nanoleaf-wpf/Helpers/EnumLocalizer.cs
+++ Nanoleaf-wpf/Helpers/EnumLocalizer.cs
@@ -5,6 +5,11 @@ namespace Winleafs.Wpf.Helpers
 {
     public static class EnumLocalizer
     {
+        /// <summary>
+        /// Localizes the enum value using the <see cref="EnumResources"/>.
+        /// </summary>
+        /// <param name="enumValue">The value to be localized.</param>
+        /// <returns>The localized string for the <see cref="enumValue"/>.</returns>
         public static string GetLocalizedEnum(Enum enumValue)
         {
             try
