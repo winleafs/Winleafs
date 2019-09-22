7d17d657b782ebfeb2618d8cabfb42c5de5c2664
Bart de Bever <bartdebever112@gmail.com>
2019-09-22 18:07:15 +0200

    Return completed task instead of turn off nanoleaf

--- Nanoleaf-wpf/Api/Effects/CustomColorEffect.cs
+++ Nanoleaf-wpf/Api/Effects/CustomColorEffect.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
@@ -31,7 +32,7 @@ namespace Winleafs.Wpf.Api.Effects
 
         public Task Deactivate()
         {
-            return _nanoleafClient.StateEndpoint.SetStateAsync(false);
+            return Task.CompletedTask;
         }
 
         public bool IsContinuous()
