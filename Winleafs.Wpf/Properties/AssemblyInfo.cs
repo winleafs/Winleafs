080c9c0d865d9084013acf87864ed96fe807d936
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2019-10-03 20:34:06 +0200

    renamed assembly name and title

--- Winleafs.Wpf/Properties/AssemblyInfo.cs
+++ Winleafs.Wpf/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Nanoleaf-wpf")]
+[assembly: AssemblyTitle("Winleafs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Nanoleaf-wpf")]
+[assembly: AssemblyProduct("Winleafs")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
