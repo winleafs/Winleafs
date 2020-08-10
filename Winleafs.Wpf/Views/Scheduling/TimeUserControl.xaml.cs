919cbd046aecb8835a6b6a38344860392d4686c3
Stijn Oostdam <stijn.oostdam@xs4all.nl>
2020-08-10 19:29:48 +0200

    In between commit

---
+++ Winleafs.Wpf/Views/Scheduling/TimeUserControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Winleafs.Wpf.Views.Scheduling
+{
+    /// <summary>
+    /// Interaction logic for TimeUserControl.xaml
+    /// </summary>
+    public partial class TimeUserControl : UserControl
+    {
+        public TimeUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
