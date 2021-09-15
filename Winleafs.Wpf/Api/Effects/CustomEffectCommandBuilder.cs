dfff735a0b9a78f4d8a2ca196f669f8c1c8de42c
Jason Steele <jasonbsteele@live.co.uk>
2021-09-15 18:54:06 +0100

    Added lock to the CustomEffectCommandBuilder

--- Winleafs.Wpf/Api/Effects/CustomEffectCommandBuilder.cs
+++ Winleafs.Wpf/Api/Effects/CustomEffectCommandBuilder.cs
@@ -13,43 +13,109 @@ namespace Winleafs.Wpf.Api.Effects
 	/// </summary>
 	public class CustomEffectCommandBuilder
 	{
+		private CustomEffect _customEffect;
+		private int _transitionTime;
 
-		public CustomEffectCommand Build(CustomEffect customEffect, string Name)
+		public CustomEffectCommandBuilder(CustomEffect customEffect)
+		{
+			_customEffect = customEffect;
+		}
+
+		public CustomEffectCommand Build(float transitionTime, string Name)
 		{
 			// If a name has been passed the custom effect will be added to the device
 			var customEffectCommand = new CustomEffectCommand();
 			customEffectCommand.Command = "add";
 			customEffectCommand.AnimName = Name;
+			
 
-
+			BuildBody(customEffectCommand);
 
 			return customEffectCommand;
 		}
 
-		public CustomEffectCommand Build(CustomEffect customEffect)
+		public CustomEffectCommand Build(float transitionTime)
 		{
 			// If no name has been passed the custom effect will just be dispayed on the device
 			var customEffectCommand = new CustomEffectCommand();
 			customEffectCommand.Command = "display";
+			_transitionTime = (int)Math.Floor(transitionTime * 10);
 
-			BuildBody(customEffect, customEffectCommand);
+			BuildBody(customEffectCommand);
 
 			return customEffectCommand;
 		}
 
-		private void BuildBody(CustomEffect customEffect, CustomEffectCommand customEffectCommand)
+		private void BuildBody(CustomEffectCommand customEffectCommand)
 		{
-			if (customEffect.IsStatic)
+
+			if (_customEffect.IsStatic)
 			{
 				customEffectCommand.AnimType = "static";
 			}
 			else
 			{
 				customEffectCommand.AnimType = "custom";
-				customEffectCommand.Loop = customEffect.IsLoop;
+				customEffectCommand.Loop = _customEffect.IsLoop;
 			}
 
+			var animData = new StringBuilder();
+			animData.AppendFormat("{0}; n", _customEffect.Frames[0].PanelColors.Count);
+
+			foreach (var panelId in _customEffect.Frames[0].PanelColors.Keys)
+			{
+				animData.Append(BuildPanelAnimData(panelId));
+			}
 			//TODO Build animdata
 		}
+
+		private string BuildPanelAnimData(int panelId)
+		{
+			var sb = new StringBuilder();
+			uint? prevRgb = null;
+
+			var totalFrames = 0;
+			var frameCount = 0;
+
+			foreach (var rgb in _customEffect.Frames.Select(f => f.PanelColors[panelId]))
+			{
+				if (prevRgb == null || rgb != prevRgb.Value)
+				{
+					if (prevRgb != null)
+					{
+						sb.AppendFormat(" {0} {1} {2} 0 {3}",
+						(prevRgb >> 16) & 255,
+						(prevRgb >> 8) & 255,
+						prevRgb & 255,
+						frameCount * _transitionTime);
+
+						totalFrames++;
+					}
+
+					sb.AppendFormat(" {0} {1} {2} 0 {3}",
+						(rgb >> 16) & 255,
+						(rgb >> 8) & 255,
+						rgb & 255,
+						_transitionTime);
+
+					frameCount = 0;
+					totalFrames++;
+				}
+				else
+				{
+					frameCount++;
+				}
+
+				prevRgb = rgb;
+			}
+
+			//prepend panelId; numframes
+			return string.Format("{0} {1}{2}", panelId, totalFrames, sb);
+		}
+		//numPanels;
+		//panelId0; numFrames0; RGBWT01; RGBWT02; ... RGBWT0n(0);
+		//panelId1; numFrames1; RGBWT11; RGBWT12; ... RGBWT1n(1); ... ...
+		//panelIdN; numFramesN; RGBWTN1; RGBWTN2; ... RGBWTNn(N);
+
 	}
 }
